5019a14c697f36870af04bfcde16687eb4045b4c
EricBlaney <EricBlaney@gmail.com>
2021-05-31 12:23:02 -0400

    Finalized first draft

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,6 +1,13 @@
 class KittensController < ApplicationController
   def index
     @kitten = Kitten.all
+
+    flash.now[:notice] = "We have exactly #{@kitten.size} kitties registered!"
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render :json => @kitten.to_json }
+    end
   end
 
   def new
@@ -13,23 +20,42 @@ class KittensController < ApplicationController
       redirect_to @kitten, notice:  "Successful Kitten Kreation.  Meow."
     else
       redirect_to new_kitten_path, alert: "Failed Kitten Kreation.  Meow."
+    end
   end
 
   def show
+    @kitten = Kitten.find(params[:id])
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render :json => @kitten.to_json }
+    end
   end
+  
 
 
   def edit
+    @kitten = Kitten.find(params[:id])
   end
 
   def update
+    @kitten = Kitten.find(params[:id])
+    if @kitten.update(kitten_params)
+      redirect_to @kitten, notice: "Kitty updated!  Meow"
+    else
+      redirect_to edit_kitten_path, alert: "Kitty update failed!  Oh no.  Meow."
+    end
   end
 
   def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+    
+    redirect_to root_path, notice: "Kitty gone now.  Meow"
   end
 
   private
   def kitten_params
-    params.require(:kitten).permit(:name, :aget, :cuteness, :softness)
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'kittens/index'
-  get 'kittens/get'
-  get 'kittens/new'
-  get 'kittens/show'
-  get 'kittens/post'
-  get 'kittens/edit'
-  get 'kittens/put'
-  get 'kittens/delete'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'kittens#index'
+  resources :kittens
 end
